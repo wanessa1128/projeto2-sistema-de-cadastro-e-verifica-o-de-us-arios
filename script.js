4c1d50ede3d4a43cf71b55d0947675e7800b4fbd
Wanessa Silva <wanessa.silva33@aluno.ce.gov.br>
2025-08-26 14:05:02 +0000

    criado script e index

---
+++ script.js
@@ -0,0 +1,21 @@
+// Casdastro de usuarios
+let nome = prompt("Digite seu nome")
+let idade = parseInt(prompt("Digite sua idade"))
+
+if(idade < 13) {
+
+} else {
+    alert(`Desculpe, idade minima 13 anos!`)
+} 
+
+let senha = parseInt(prompt("Digite sua senha"))
+// Login
+let nomeNovamente = prompt("Digite seu nome novamente")
+let senhaNovamente = parseInt(prompt("Digite sua senha"))
+
+if(nomeNovamente == nome && senhaNovamente == senha) {
+
+alert("Login realizado com sucesso!")    
+
+} else (senhaNovamente === senha && nomeNovamente === nome)
+  alert("Nome ou senha invalidos!")
