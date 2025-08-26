8f00d08d9c7657cecc5497756f6c664bd4b8b8e9
Wanessa Silva <wanessa.silva33@aluno.ce.gov.br>
2025-08-26 14:36:24 +0000

    adulterado script

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 // Casdastro de usuarios
 let nome = prompt("Digite seu nome:")
 let idade = parseInt(prompt("Digite sua idade:"))
+let senha = parseInt(prompt("Digite sua senha:"))
 
-if(idade < 13) {
+if(idade <= 13) {
 
 } else {
-    alert(`Desculpe, idade minima 13 anos!`)
-} 
-
-let senha = parseInt(prompt("Digite sua senha:"))
-// Login
-let nomeNovamente = prompt("Digite seu nome novamente:")
+   let nomeNovamente = prompt("Digite seu nome novamente:")
 let senhaNovamente = parseInt(prompt("Digite sua senha:"))
 
 if(nomeNovamente == nome && senhaNovamente == senha) {
@@ -19,3 +15,7 @@ alert(`Login realizado com sucesso ${nome}!`)
 
 } else (senhaNovamente === senha && nomeNovamente === nome)
   alert("Nome ou senha invalidos!")
+} 
+
+
+
